094792fc6886b8e11bd1407058543af7b7d7cfb6
fernandothiers <fernandothiers@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-08 03:46:12 +0000

    commit apenas para dar merge com o hc

--- src/lib.c
+++ src/lib.c
@@ -50,3 +50,38 @@ int toUpper( char *string ) {
 	}
 	return cont;
 }
+
+int wordCount( Word **string ){
+
+	Word *str1, *str2;
+
+	str1 = str2 = *string;
+
+	if( str1 == NULL ) return 0;
+
+	str1->count = 1;
+
+	while( str2 != NULL ){
+
+	str1 = *string;
+
+		while( str1 != str2 ){
+			if( str1->info != str2->info )
+				str1 = str1->next;
+			else{
+				str1->count++;
+				delWord( &str1, str2 );
+			}
+				
+
+	
+
+
+
+	str2 = str2->next;
+
+	}
+
+int delWord( Word **begin, Word *t ){
+
+if( (*begin)->next == t ) 
