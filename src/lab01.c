523993c44534c918ad1877fae16fab21dd7c62fe
fernandothiers <fernandothiers@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-08 01:43:02 +0000

    isAlpha,toUpper,toBlank funcionando

--- src/lab01.c
+++ src/lab01.c
@@ -1,1 +1,24 @@
-//lab01.c
+/*lab01.c */
+
+#include "lib.h"
+
+int main(){
+
+	char string[]="O HC ! 8654 eh uma BiXo232Na $ e g1os1ta de D2AR o CU3!!!",
+	     separators[]="?$";
+
+	printf("%s\n\n", string);
+
+	toUpper(string);
+
+	printf("%s\n\n", string);
+
+	toBlank(string, separators);
+
+	printf("%s\n\n", string);
+
+	return 0;
+}
+
+
+
--- src/lib.c
+++ src/lib.c
@@ -1,7 +1,43 @@
-//lib.c
+/*lib.c*/
+
+int isAlphaNumeric( char c ){
+
+	if( (c>='A' && c<='Z') || (c>='0' && c<='9') ) return 1;
+	return 0;
+
+}
+
+int toBlank( char *string, char *separators ){
+
+	char *str, *sep;
+
+	str = string;
+	sep = separators;
+
+	/*Se houver separadores*/
+	if( *separators != '\0' ){
+
+		while(1){
+			str += strcspn( str, sep );
+			if( *str != '\0' ) *str = ' ';
+			else return 1;
+		}
+	}
+
+	/*Se não houver separadores*/
+	else{
+
+		while( str != '\0' ){
+			if( !isAlphaNumeric(*str) ) *str = ' ';
+			str++;
+		}
+		return 1;
+	}
+
+	return 0;
+}
 
 int toUpper( char *string ) {
-
 
 	int cont;
 
@@ -12,6 +48,5 @@ int toUpper( char *string ) {
 		}
 		string++;
 	}
-
 	return cont;
 }
