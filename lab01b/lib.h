4fdf930d555f0ed40d514edebcba321595b8415c
fernandothiers <fernandothiers@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-22 19:40:25 +0000

    upada lib.h comentada pelo hc

--- lab01b/lib.h
+++ lab01b/lib.h
@@ -1,9 +1,11 @@
 /*lib.h*/
 
+// Definicioes de bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Definicioes de constantes
 #define SEPARATORS ",;!.?():"
 #define BLANK " "
 
@@ -13,6 +15,12 @@
 #define CHANGECHR '-'
 #define CHANGECHR2 '_'
 
+/* Estrutura da lista de palavras
+ * char *info : string com conteudo da palavra
+ * int count : quantidade de vezes que a palavra eh repetida
+ * struct tWord *next : apontador para proximo elemento da lista 
+ * responsavel: Felipe/Fernando
+ */
 typedef struct tWord{
 
 	char *info;
@@ -21,46 +29,86 @@ typedef struct tWord{
 
 } Word;
 
+
+/* Verifica se o caracter eh alfanumerico
+ * paramentro 1: caracter a ser verificado
+ * retorna TRUE caso afirmativo; retorna FALSE caso negativo
+ * responsavel Fernando
+ */
 int isAlphaNumeric( char );
 
-/* O 2º parametro é a lista de separadores, no caso de ser vazio a função roda com "not-alfanumericos" */
-int toBlank( char*, char* );
+/* Converte os caracteres do segundo argumento de uma string para espacos em branco
+ * parametro 1: string a ter os separadores convertidos para espacos em branco
+ * parametro 2: lista de separadores, no caso de ser vazio a funcao roda com "not-alfanumeric"
+ * responsavel Fernando
+ * */
+void toBlank( char*, char* );
 
+/* Converte os caracteres de uma string para maiusculos
+ * parametro 1: string a ser masculinizada =)
+ * retorna quantidade de caracteres convertidos
+ * responsavel Fernando
+ */
 int toUpper( char* );
 
+/* Separa uma string em uma lista de Words a partir dos espacos em branco
+ * parametro 1: string a ser separada
+ * parametro 2: lista de Words
+ * retorna quantidade total de elementos finais da lista de Words
+ * responsavel Felipe
+ */
 int blankSplit( char*, Word** );
 
+
+/* A partir da lista de Words, conta o total de palavras diferentes que ela possui, "agrupando" as repetidas em elementos Word da lista
+ * paramentro 1: lista de Words
+ * retorna o total de palavras diferentes
+ * responsavel Fernando
+ */
 int wordCount( Word** );
 
-int delWord( Word*, Word** );
-
+/* Imprime o arquivo de palavras da lista de Word
+ * parametro 1: Lista de Word
+ * parametro 2: nome do arquivo a ser gerado
+ * retorna TRUE caso o arquivo seja gerado com sucesso ou FALSE caso nao seja gerado o arquivo.
+ * responsavel Jonathan
+ */
 int printFile( Word*, char* );
 
-int printWord( Word* );
+/* Imprime a lista de Word na saida padrao
+ * parametro 1: Lista de Word
+ * responsavel Felipe
+ */
+void printWord( Word* );
 
+/* Conta a quantidade de vezes que um caracter se repete  numa lista de Word
+ * parametro 1: Lista de Word
+ * parametro 2: caracter a ser contado
+ * retorna a quantidade de vezes que o caracter se repete
+ * responsavel: Jonathan
+ */
 int cCountWord ( Word *, char );
 
+/* Conta a quantidade de vezes que uma palavra se repete numa lista de Word
+ * parametro 1: Lista de Word
+ * parametro 2: palavra a ser contada
+ * retorna a quantidade de vezes que a palavra se repete
+ * responsavel: Jonathan
+ */
 int sCountWord ( Word *, char *);
 
+/* Conta a quantidade de vezes que um caracter se repete numa string
+ * parametro 1: string
+ * parametro 2: palavra a ser contada
+ * retorna a quantidade de vezes que o caracter se repete
+ * responsavel: Felipe
+ */
 int cCount ( char *, char );
 
+/* Conta a quantidade de vezes que uma palavra se repete numa lista de Word
+ * parametro 1: Lista de Word
+ * parametro 2: palavra a ser contada
+ * retorna a quantidade de vezes que a palavra se repete
+ * responsavel: Felipe
+ */
 int sCount ( char *, char * );
-
-int countCharsFile(char *);
-
-/*A funcao retorna um ponteiro para a palavra separada
-Parametros:
--char **str: ponteiro a ser alterado para parar no final de cada palavra separada
--char *SEP: string com os separadores;
-Feita por: Jonathan Nunes Boilesen
-2:11 a.m. 19/03/2009*/
-char *sepString (char **, char *);
-
-/*Funcao que busca as informacoes contidas
-no arquivo de configuracao ini.conf
-Recebe
--char *inf
-Contendo uma string com a informacao desejada de retorno
-Feita por: Jonathan Nunes Boilesen
-23:00 p.m. 20/03/2009*/
-char *getConfig(char *)
