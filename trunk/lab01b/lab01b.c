34fcc5288353cca9447d2f6dcdd1ab040244ad09
fernandothiers <fernandothiers@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-23 00:13:33 +0000

    acrescentado ptbr.lan e formatada a main para executar testes

--- trunk/lab01b/lab01b.c
+++ trunk/lab01b/lab01b.c
@@ -12,18 +12,12 @@ Input inputTest(int argc, char *argv[]){
 
 	// Testa o numero de argumentos
 	if( argc == 2 ){ // Caso em que é passado apenas 1 arquivo como argumento
-		if( (file = fopen(argv[1],"r")) == NULL ){
-			printf("Arquivo não encontrado.\n");
-			return ERROR;
-		}
+		if( (file = fopen(argv[1],"r")) == NULL ) return ERROR;
 		else return t_file;
 	}
 	if( argc == 4 ){
 		// Testa se o 3 argumento é um caracter
-		if( strlen(argv[2]) != 1  ){
-			printf("3o argumento deve ser um caracter.\n");
-			return ERROR;
-		}
+		if( strlen(argv[2]) != 1  ) return ERROR;
 		else return text_c_pal;
 	}
 	return ERROR;
@@ -34,7 +28,7 @@ int read_file( FILE *file, char *sep, char **text, char *c, char **pal ){
 	char *string, *cp_string, *caracter;
 	int i, stringsize;
 
-	// Le conteúdo do arquivo e armazena em string
+	// Le conteudo do arquivo e armazena em string
 	stringsize = fileStringSize( *file, '\n' );
 	string = (char *) malloc( ( stringsize  + 1 ) * sizeof(char) );
 	for( i=0; i<stringsize; i++) string[i] = getc(file);
@@ -73,24 +67,36 @@ int main(int argc, char *argv[]){
 	Input input; // Recebe o tipo da entrada
 	char *text, c, *pal, *filename, *sep, *lang, ***messages;
 	Word *word;
-	int i;
 	FILE *file;
-
-	// Verificação da entrada
-	input = inputTest(argc, argv);
-	if( input == 0 ) exit(1); 
-
-	// Le string de separadores do arquivo de configuração, getConfig aloca,     	 		LEMBRAR DE DAR O FREE
-	sep = getConfig( "SEP" );
-	if( sep == NULL ) exit(1);
 
 	// Le idioma do arquivo de configuração, getConfig aloca,                   		 	LEMBRAR DE DAR O FREE
 	lang = getConfig( "LANG" );
-	if( lang == NULL ) exit(1);	
+	if( lang == NULL ){
+		printf("ERROR! The program could not read the language from the configuration file.\n\n");
+		exit(1);
+	}
 	
 	// Carrega mensagens do idioma, loadMessages aloca,						FREE!
 	messages = loadMessages( lang );
-	if( messages == NULL ) exit(1);
+	if( messages == NULL ){
+		printf("ERROR! The program could not read the language file.\n\n");
+		exit(1);
+	}
+	printMsg( messages, MSG, 1 );
+
+	// Verificação da entrada
+	input = inputTest(argc, argv);
+	if( input == 0 ){
+		printMsg( messages, ERR, 1 ); // "Arquivo não encontrado ou entrada em formato incompativel"
+		exit(1);
+	}
+
+	// Le string de separadores do arquivo de configuração, getConfig aloca,     	 		LEMBRAR DE DAR O FREE
+	sep = getConfig( "SEP" );
+	if( sep == NULL ){
+		printMsg( messages, ERR, 2 ); // "ERRO! Arquivo de configuracao mal formatado.
+		exit(1);
+	}
 
 	// Entrada do tipo texto+caracter+palavra
 	if( input == text_c_pal ){
@@ -103,45 +109,59 @@ int main(int argc, char *argv[]){
 		pal = (char*) malloc( strlen(argv[3]) * sizeof(char));                 			//FREE!
 		strcpy( pal, argv[3] );
 	}
-
 	// Entrada do tipo arquivo
 	if( input == t_file ){
 		// Abre o arquivo, le entrada, fecha o arquivo
 		file = fopen( argv[1], "r" );
-		if( read_file( file, sep, &text, &c, &pal ) == 0 ) exit(1);
+		if( read_file( file, sep, &text, &c, &pal ) == 0 ){
+			printMsg( messages, ERR, 3 ); // "Arquivo de entrada mal formatado"
+			exit(1);
+		}
 		fclose( file );
 	}
+	printMsg( messages, MSG, 2 );
 
-	printf("\n->Texto\n%s\n\n", text);
+	// Imprime texto original
+	printMsg( messages, MSG, 3 );
+	printf("%s\n\n", text);
 
-	// Imprime a quantidade de vezes que o caracter c e 
-	// a string pal se repetem no texto
-	printf("->Quantidade de vezes que \"%c\" se repete no texto\n%d\n\n", c, cCount(text, c));
-	printf("->Quantidade de vezes que \"%s\" se repete no texto\n%d\n\n", pal, sCount(text, pal));
-
+	// Imprime a quantidade de vezes que o caracter c e a string pal se repetem no texto
+	printMsg( messages, MSG, 4 );
+	printf(" %d\n\n", cCount(text, c));
+	printMsg( messages, MSG, 5 );
+	printf(" %d\n\n", sCount(text, pal));
+	
+	// Imprime texto em maiusculo
 	toUpper(text);
-	printf("->Texto em maiusculo\n%s\n\n", text);
+	printMsg( messages, MSG, 6 );
+	printf("%s\n\n", text);
 
+	// Imprime texto com caracteres especiais transformados em espaco
 	toBlank(text, NULL);
-	printf("->Texto com caracteres especiais transformados em espaco\n%s\n\n", text);
+	printMsg( messages, MSG, 7 );
+	printf("%s\n\n", text);
 
-	i = blankSplit(text, &word);
-	printf("->Numero de palavras do texto\n%d\n\n", i);
+	// Imprime numero de palavras do texto
+	printMsg( messages, MSG, 8 );
+	printf(" %d\n\n",blankSplit( text, &word));
 
-	i = wordCount(&word);
-	printf("->Numero de palavras diferentes do texto\n%d\n\n",i);
+	// Imprime numero de palavras diferentes do texto
+	printMsg( messages, MSG, 9 );
+	printf(" %d\n\n",wordCount( &word ));
 
-	printf("->Impressao da lista de palavras\n");
+	// Imprime a lista de palavras
+	printMsg( messages, MSG, 10 );
 	printWord(word);
 	printf("\n");
 
-	printf("->Gerando arquivo com lista de palavras...");
-	//  pal.txt
+	// Gera arquivo com a lista de palavras pal.txt
+	printMsg( messages, MSG, 11 );
 	filename = (char*) malloc((strlen(pal)+3)*sizeof(char));
 	filename = strcat(filename, pal);
 	filename = strcat(filename, ".txt");
 	printFile(word, filename);
-	printf(" arquivo criado \"%s\"\n\n", filename);
+	printMsg( messages, MSG, 12 );
+	printf(" \"%s\"\n\n", filename);
 
 	return 0;
 }
