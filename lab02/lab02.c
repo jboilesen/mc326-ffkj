c32fb8df8387819640f977e0e7648116dfab9d64
richardhb <richardhb@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-29 18:49:36 +0000

    funcao GERA_ARQ_COM_SEPARADOR funcionando para leitura em loop

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define RA_size 7
 #define NOME_size 25
 #define CIDADE_size 16
@@ -6,11 +9,14 @@
 #define TELALT_size 9
 #define SEXO_size 1
 #define CURSO_size 3
+
+
 void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
 {
         char ra[RA_size], nome[NOME_size], cidade[CIDADE_size];
         char telres[TELRES_size], telalt[TELALT_size],sexo,curso[CURSO_size], aux;
         int i, size =0;
+        while(1){//Captura todos os dados dos campos do arquivo 'file' e grava no arquivo 'novo'
               for(i=0;i<RA_size-1;i++){
                   ra[i] = fgetc(file);
               }
@@ -23,6 +29,8 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
                   nome[i] = fgetc(file);
               }
               nome[i]='\0';
+              size=strlen(nome);
+              printf("size of nome:%d\n",size);
               printf("string nome:%s\n",nome);
               
               for(i=0;i<CIDADE_size-1;i++){
@@ -61,6 +69,10 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
               aux = fgetc(file);
               printf("aux:%c\n",aux);
               
+              aux = fgetc(file);//Verifica se chegou a um '\n' ou a fim do arquivo
+              if(aux==EOF)
+                    break;
+         }
 }
 
 int main(int argc, char *argv[]){
