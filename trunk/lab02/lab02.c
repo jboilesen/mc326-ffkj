2675368bb687849c01d3cefb2bc6113768db465b
richardhb <richardhb@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-29 19:11:46 +0000

    funcao GERA_ARQ_COM_SEPARADOR funcionando para leitura e escrita em loop

--- trunk/lab02/lab02.c
+++ trunk/lab02/lab02.c
@@ -9,6 +9,9 @@
 #define TELALT_size 9
 #define SEXO_size 1
 #define CURSO_size 3
+#define separador '|'
+#define blank ' '
+#define fimregistro '#'
 
 
 void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
@@ -17,6 +20,8 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
         char telres[TELRES_size], telalt[TELALT_size],sexo,curso[CURSO_size], aux;
         int i, size =0;
         while(1){//Captura todos os dados dos campos do arquivo 'file' e grava no arquivo 'novo'
+        
+              //Parte 1 - Leitura
               for(i=0;i<RA_size-1;i++){
                   ra[i] = fgetc(file);
               }
@@ -29,7 +34,6 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
                   nome[i] = fgetc(file);
               }
               nome[i]='\0';
-              size=strlen(nome);
               printf("size of nome:%d\n",size);
               printf("string nome:%s\n",nome);
               
@@ -70,8 +74,57 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo)
               printf("aux:%c\n",aux);
               
               aux = fgetc(file);//Verifica se chegou a um '\n' ou a fim do arquivo
+              
+              //Parte 2 - Escrita
+              for(i=0;i<RA_size-1;i++){
+                  fputc(ra[i],novo);
+              }
+              fputc(separador,novo);
+              
+               for(i=0;i<NOME_size-1;i++){
+                  if(nome[i]==blank && nome[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(nome[i],novo);
+              }
+              fputc(separador,novo);
+
+              for(i=0;i<CIDADE_size-1;i++){
+                  if(cidade[i]==blank && cidade[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(cidade[i],novo);
+              }
+              fputc(separador,novo);
+              
+              for(i=0;i<TELRES_size-1;i++){
+                   if(telres[i]==blank && telres[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(telres[i],novo);
+              }
+              fputc(separador,novo);
+              
+              for(i=0;i<TELALT_size-1;i++){
+                   if(telalt[i]==blank && telalt[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(telalt[i],novo);
+              }
+              fputc(separador,novo);
+              
+              fputc(sexo,novo);
+              
+              fputc(separador,novo);
+              
+              for(i=0;i<CURSO_size-1;i++){
+                   if(curso[i]==blank && curso[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(curso[i],novo);
+              }
+              fputc(separador,novo);
+              
+              fputc(fimregistro,novo);              
               if(aux==EOF)
                     break;
+              else
+                    fputc('\n',novo);
          }
 }
 
@@ -84,6 +137,8 @@ int main(int argc, char *argv[]){
         new = fopen(argv[2],"w");
         if(file){
               GERA_ARQ_COM_SEPARADOR(file, new);
+              fclose(file);
+              fclose(new);
         }
         else
             return 1;
