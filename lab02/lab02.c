48b5a3349f76e88b8e61ee20548fda810dfecee1
richardhb <richardhb@8f2ee822-0b60-11de-b5bf-434079a3519f>
2009-03-29 21:18:07 +0000

    modificacoes na funcao GERA_ARQ_COM_SEPARADOR

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -12,13 +12,14 @@
 #define separador '|'
 #define blank ' '
 #define fimregistro '#'
+#define TRUE 1
+#define FALSE 0
 
 /*Funcao que le um campo de registro(tamanho fixo) a partir de um arquivo
 -Parametro 1: string a ser preenchida
 -Parametro 2: tamanho da string a ser lida
 -Parametro 3: Contador de numero de caracteres
 -Parametro 4: Arquivo a ser lido
-Contendo uma string com a informacao da linguagem utilizada palo programa
 Feita por: Richard Keller
 16:56 p.m. 29/03/2009*/
 char* leituraCampoFixo(char* word,int size,int* NcaracteresEntrada1, FILE* file){
@@ -32,6 +33,25 @@ char* leituraCampoFixo(char* word,int size,int* NcaracteresEntrada1, FILE* file)
         return word;
 }
 
+/*Funcao que grava um campo de registro ,retirando o espaços a direita da string, em um arquivo
+-Parametro 1: string a ser gravada
+-Parametro 2: tamanho da string a ser gravada
+-Parametro 3: Contador de numero de caracteres
+-Parametro 4: Arquivo a ser escrito
+Feita por: Richard Keller
+18:15 p.m. 29/03/2009*/
+void gravaCampoComSep(char* word,int size,int* Ncaracteres1, FILE* novo){
+        int i;
+        for(i=0;i<size-1;i++){
+                  if(word[i]==blank && word[i+1]==blank)// Pensar em solução melhor para isso
+                      break;
+                  fputc(word[i],novo);
+                  *Ncaracteres1+=1;
+        }
+        fputc(separador,novo);
+        *Ncaracteres1+=1;
+}
+
 /*Funcao que le um arquivo de registro de tamanho fixo e grava em outro arquivo usando separador
 -Parametro 1: arquivo a ser lido
 -Parametro 2: arquivo a ser gravado
@@ -39,9 +59,12 @@ char* leituraCampoFixo(char* word,int size,int* NcaracteresEntrada1, FILE* file)
 -Parametro 4: Contador de numero de registros da entrada
 -Parametro 5: Contador de numero de caracteres da saida
 Contendo uma string com a informacao da linguagem utilizada palo programa
+
+-Retorna False se número de parametros ou posição dos parametros estiver errada
+-Senão retorna True
 Feita por: Richard Keller
 16:56 p.m. 29/03/2009*/
-void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo, int* Nregistro, int* NcaracteresEntrada1, int* Ncaracteres1)
+int GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo, int* Nregistro, int* NcaracteresEntrada1, int* Ncaracteres1)
 {
         char ra[RA_size], nome[NOME_size], cidade[CIDADE_size];
         char telres[TELRES_size], telalt[TELALT_size],sexo,curso[CURSO_size], aux;
@@ -49,124 +72,68 @@ void GERA_ARQ_COM_SEPARADOR(FILE* file, FILE* novo, int* Nregistro, int* Ncaract
         while(1){//Captura todos os dados dos campos do arquivo 'file' e grava no arquivo 'novo'
         
               //Parte 1 - Leitura
-              
               strcpy(ra,leituraCampoFixo(ra,RA_size,NcaracteresEntrada1, file));
-              //printf("string ra:%s\n",ra);
               
-              aux = fgetc(file);
+              aux = fgetc(file);//leitura de espaço em branco
               *NcaracteresEntrada1 +=1;
               
               strcpy(nome,leituraCampoFixo(nome,NOME_size,NcaracteresEntrada1, file));
-
-              //printf("size of nome:%d\n",size);
-              //printf("string nome:%s\n",nome);
-              
               strcpy(cidade,leituraCampoFixo(cidade,CIDADE_size,NcaracteresEntrada1, file));
-
-              //printf("string cidade:%s\n",cidade);
-              
               strcpy(telres,leituraCampoFixo(telres,TELRES_size,NcaracteresEntrada1, file));
-
-              //printf("string telres:%s\n",telres);
-              
-              aux = fgetc(file);
+              aux = fgetc(file);//leitura de espaço em branco
+              *NcaracteresEntrada1 +=1;
               
               strcpy(telalt,leituraCampoFixo(telalt,TELALT_size,NcaracteresEntrada1, file));
-
-              //printf("string telalt:%s\n",telalt);
-              
-              aux = fgetc(file);
+              aux = fgetc(file);//leitura de espaço em branco
               *NcaracteresEntrada1 +=1;
               
               sexo = fgetc(file);
               *NcaracteresEntrada1 +=1;
-              //printf("string sexo:%c\n",sexo);
               
-              aux = fgetc(file);
+              aux = fgetc(file);//leitura de espaço em branco
               *NcaracteresEntrada1 +=1;
               
               strcpy(curso,leituraCampoFixo(curso,CURSO_size,NcaracteresEntrada1, file));
-
-              //printf("string curso:%s\n",curso);
               
               aux = fgetc(file);
               *NcaracteresEntrada1 +=1;
-              //printf("aux:%c\n",aux);
               
-              aux = fgetc(file);//Verifica se chegou a um '\n' ou a fim do arquivo
+              aux = fgetc(file);
               *NcaracteresEntrada1 +=1;
               
               //Parte 2 - Escrita
-              for(i=0;i<RA_size-1;i++){
-                  fputc(ra[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
-              
-               for(i=0;i<NOME_size-1;i++){
-                  if(nome[i]==blank && nome[i+1]==blank)// Pensar em solução melhor para isso
-                      break;
-                  fputc(nome[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
-
-              for(i=0;i<CIDADE_size-1;i++){
-                  if(cidade[i]==blank && cidade[i+1]==blank)// Pensar em solução melhor para isso
-                      break;
-                  fputc(cidade[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
-              
-              for(i=0;i<TELRES_size-1;i++){
-                   if(telres[i]==blank && telres[i+1]==blank)// Pensar em solução melhor para isso
-                      break;
-                  fputc(telres[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
-              
-              for(i=0;i<TELALT_size-1;i++){
-                   if(telalt[i]==blank && telalt[i+1]==blank)// Pensar em solução melhor para isso
-                      break;
-                  fputc(telalt[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
+              gravaCampoComSep(ra, RA_size, Ncaracteres1, novo);
+              gravaCampoComSep(nome, NOME_size, Ncaracteres1, novo);
+              gravaCampoComSep(cidade,CIDADE_size,Ncaracteres1, novo);
+              gravaCampoComSep(telres,TELRES_size,Ncaracteres1,novo);
+              gravaCampoComSep(telalt,TELALT_size,Ncaracteres1,novo);
               
               fputc(sexo,novo);
+              printf("i=%d",i);
               *Ncaracteres1+=1;
-              
               fputc(separador,novo);
               *Ncaracteres1+=1;
               
-              for(i=0;i<CURSO_size-1;i++){
-                   if(curso[i]==blank && curso[i+1]==blank)// Pensar em solução melhor para isso
-                      break;
-                  fputc(curso[i],novo);
-                  *Ncaracteres1+=1;
-              }
-              fputc(separador,novo);
-              *Ncaracteres1+=1;
-              
+              gravaCampoComSep(curso,CURSO_size,Ncaracteres1,novo);
               fputc(fimregistro,novo);
-              *Ncaracteres1+=1;              
-              if(aux==EOF){
+              *Ncaracteres1+=1;      
+                      
+                      
+              if(aux==EOF){//Verifica se chegou a um '\n' ou a fim do arquivo
                     *Nregistro+=1;
                     break;
               }
-              else{
+              else if(aux=='\n'){
                     fputc('\n',novo);
                     *Ncaracteres1+=1;
                     *Nregistro+=1;
-                    }
-         }
+              }
+              else{
+                    //mensagem de ERRO
+                    return FALSE;
+              }
+        }
+        return TRUE;
 }
 
 int main(int argc, char *argv[]){
@@ -179,7 +146,10 @@ int main(int argc, char *argv[]){
         file = fopen(argv[1],"r");
         new = fopen(argv[2],"w");
         if(file){
-              GERA_ARQ_COM_SEPARADOR(file, new, &Nregistro, &NcaracteresEntrada1, &Ncaracteres1);
+              if(GERA_ARQ_COM_SEPARADOR(file, new, &Nregistro, &NcaracteresEntrada1, &Ncaracteres1) == FALSE){
+                    //MENSAGEM DE ERRO
+                    return 1;
+              }
               fclose(file);
               fclose(new);
               printf("Numero de registros do arquivo 1: %d\n",Nregistro);
